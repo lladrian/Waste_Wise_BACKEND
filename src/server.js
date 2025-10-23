d1f83293ecede3f9ffff9a9e498aa0683d619bfb
Adrian Manatad <lladrian@users.noreply.github.com>
2025-10-23 09:08:27 +0800

    cors

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ import requestRoutes from "./routes/request_route.js";
 app.use(corsMiddleware({
   //origin: (origin, callback) => callback(null, true),
   origin: '*',  // for open access, but can't use credentials:true with '*'
-  credentials: true,
+  // credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 }));
