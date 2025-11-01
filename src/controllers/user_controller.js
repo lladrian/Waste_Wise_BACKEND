77ea6a1d2ec6cb8861192590d78272684df71ae5
Adrian Manatad <lladrian@users.noreply.github.com>
2025-11-01 21:52:24 +0800

    updated profile

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -665,7 +665,8 @@ export const update_user_profile = asyncHandler(async (req, res) => {
             return res.status(400).json({ message: updateSpecificUser });
         }
 
-        return res.status(200).json({ data: 'User account successfully updated.' });
+        return res.status(200).json({ data: updateSpecificUser });
+       // return res.status(200).json({ data: 'User account successfully updated.' });
     } catch (error) {
         return res.status(500).json({ error: 'Failed to update user account.' });
     }
