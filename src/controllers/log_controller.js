489a8754e3517f07d910606f13dc45905a2dbc0d
Adrian Manatad <lladrian@users.noreply.github.com>
2025-11-12 02:58:39 +0800

    login_log

--- src/controllers/log_controller.js
+++ src/controllers/log_controller.js
@@ -21,3 +21,24 @@ export const get_all_login_log = asyncHandler(async (req, res) => {
 });
 
 
+export const get_all_login_log_specific_user = asyncHandler(async (req, res) => {
+    const { user_id } = req.params; // Get the meal ID from the request parameters
+
+    try {
+        const logs = await LoginLog.find({user: user_id })
+            .populate('user')
+            .populate({
+                path: 'user',
+                populate: {
+                    path: 'role_action',
+                    model: 'Action'
+                }
+            });
+
+        return res.status(200).json({ data: logs });
+    } catch (error) {
+        return res.status(500).json({ error: 'Failed to get all logs.' });
+    }
+});
+
+
--- src/routes/log_route.js
+++ src/routes/log_route.js
@@ -4,9 +4,11 @@ import * as LogController from '../controllers/log_controller.js'; // Import the
 const logRoutes = Router();
 
 logRoutes.get('/get_all_login_log', LogController.get_all_login_log);
+logRoutes.get('/get_all_login_log_specific_user/:user_id', LogController.get_all_login_log_specific_user);
 
 
 
+ 
 
 export default logRoutes;
 
